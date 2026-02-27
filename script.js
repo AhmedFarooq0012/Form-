f638dbddf853d44a05bce3383e55940a12e65cec
Ahmed Farooq <Ahmedfarooq00@outlook.com>
2026-02-27 12:03:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Create empty array to store all form records
+let students = [];
+
+// Select form
+let form = document.getElementById("myform");
+
+// Add event listener
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault(); // Stop page refresh
+
+    // Create object
+    let student = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        age: document.getElementById("age").value,
+        rollNumber: document.getElementById("r_number").value
+    };
+    students.push(student);
+    console.log(students);
+
+    form.reset();
+});
