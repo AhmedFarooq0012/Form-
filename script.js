5fc5eada4fb8e2af43d31b6a0740d30c8c353930
Ahmed Farooq <Ahmedfarooq00@outlook.com>
2026-03-03 11:37:45 +0500

    changes complete

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Create empty array to store all form records
 let students = [];
+let editIndex = -1;
 
 // Select form
 let form = document.getElementById("myform");
+let list = document.getElementById("list");
 
 // Add event listener
 form.addEventListener("submit", function(event) {
@@ -16,13 +18,52 @@ form.addEventListener("submit", function(event) {
         age: document.getElementById("age").value,
         rollNumber: document.getElementById("r_number").value
     };
+    
     if(student.name ==="" || student.email === "" || student.age === "" || student.rollNumber === ""){
         console.log ("please fill all the fields")
     }
-    else students.push(student);
-    console.log(students);
+    else{
+        students.push(student)
+    }
+
+    //  if (editIndex === -1) {
+    //     students.push(student);
+    // } 
+    // // UPDATE
+    // else {
+    //     students[editIndex] = student;
+    //     editIndex = -1;
+    // }
 
     form.reset();
-    console.log("data submitted successfully")
+    displayData();
 
 });
+function displayData() {
+    list.innerHTML = "";
+
+    students.forEach(function (item, index) {
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            ${item.name} - ${item.email} - ${item.age} -${item.rollNumber}
+            <button onclick="editData(${index})">Edit</button>
+            <button onclick="deleteData(${index})">Delete</button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+function editData(index) {
+    document.getElementById("name").value = students[index].name;
+    document.getElementById("email").value = students[index].email;
+    document.getElementById("age").value = students[index].age;
+    document.getElementById("r_number").value = students[index].age;
+
+    editIndex = index;
+}
+// DELETE
+function deleteData(index) {
+    students.splice(index, 1);
+    displayData();
+}
